b95aa3106aa15c5eda6b1264d806efecbcec7dad
4KevR <50447092+4KevR@users.noreply.github.com>
2022-04-26 15:49:08 +0200

    added[client]: second rfid reader, class structure

--- client/main.py
+++ client/main.py
@@ -1,29 +1,55 @@
+import time
+
 import RPi.GPIO as GPIO
-from time import sleep
-import sys
 from mfrc522 import SimpleMFRC522
 import requests
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-if __name__ == "__main__":
-    reader = SimpleMFRC522()
 
+def send_request(s_id, c_id) -> requests.Response:
+    request_url = "https://{0}/e/rfid/{1}/{2}/{3}".format("localhost", "0", s_id, c_id)
+    print(f"POST-request to {request_url}")
+    r = requests.post(request_url)
+    return r
+
+
+class RfidReader:
+    def __init__(self):
+        self.last_id = 0
+        self.scanner_ids = [111, 222]
+        self.reader = [SimpleMFRC522(0), SimpleMFRC522(1)]
+
+    def get_next_id(self) -> (int, int):
+        c_id = s_id = 0
+        while c_id == 0 or c_id is None:
+            for index, s_id in enumerate(self.scanner_ids):
+                c_id = self.reader[index].read_id_no_block()
+                start_time = time.time()
+                while c_id is None and time.time() - start_time < 0.2:
+                    c_id = self.reader[index].read_id_no_block()
+                if c_id is not None:
+                    print(f"Got id {c_id} from scanner {s_id}")
+                    break
+        return s_id, c_id
+
+
+if __name__ == "__main__":
     try:
-        last_id = 0
+        rfid_reader = RfidReader()
         while True:
             print("RFID-Reader ready...")
-            id, text = reader.read()
-            if last_id != id:
-                print("ID: %s\nText: %s" % (id, text))
-                resp = requests.post(f"{0}/e/rfid/{1}/{2}/{3}".format("localhost", "0", "0", id))
+            scanner_id, card_id = rfid_reader.get_next_id()
+            try:
+                resp = send_request(scanner_id, card_id)
                 if not resp.ok:
-                    print("Request failed")
-            else:
-                print("Same ID as last time")
-            sleep(5)
+                    print("Request failed\n")
+                else:
+                    print(f"Response: {resp.text}\n")
+            except requests.exceptions.ConnectionError:
+                print("Failed to establish a connection!\n")
+            time.sleep(1)
     except KeyboardInterrupt:
         GPIO.cleanup()
         raise
-

05b96322ae22ef263f5e07e765285c077ef2cca6
memeToasty <31214870+memeToasty@users.noreply.github.com>
2022-04-26 15:15:09 +0200

    feat[client]: client goal-script (#2)
    * added[client]: added goal-tracker script
    
    * added[client]: sending POST to backend
    
    * modified[client]: documentation
    
    * modified[client]: implemented wished changes
    
    * modified[client]: modified readme, implemented wished changes
    
    * modified[client]: implemented wished changes
    
    * modified[client]: added -
    
    * modified[client]: made standard protocol http
    
    * modified[client]: added -
    
    * modified[client]: Refactoring
    
    * refactor[client]: -
    
    ♥
    
    * Update client/goals.py
    
    Co-authored-by: TomRomeo <60541979+TomRomeo@users.noreply.github.com>
    Co-authored-by: Daniel <71837281+darmiel@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # InnoDays2022
 
-AA
+
+## Client
+
+### goal-tracking client
+
+#### Execution
+```
+usage: goals.py [-h] [--backend-url BACKEND_URL] [--kid KID]
+
+Setting up goal counter for kicker table
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --backend-url BACKEND_URL
+                        URL of Pinguin-backend
+  --kid KID              ID of kicker table
+```
---
+++ client/goals.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as GPIO
+import requests
+import argparse
+import asyncio
+
+# Argparsing
+parser = argparse.ArgumentParser(
+    description='Setting up goal counter for kicker table')
+parser.add_argument('--backend-url', type=str,
+                    default='http://localhost', help='URL of Penguin')
+parser.add_argument('--kid', type=str, default='1', help='ID of kicker table')
+
+
+BACKEND_URL = "http://localhost"
+KICKER_ID = 0
+
+GOAL_BLACK_PIN = 10
+GOAL_WHITE_PIN = 12
+
+GOAL_BLACK_ID = 1
+GOAL_WHITE_ID = 2
+
+PINS = [GOAL_BLACK_PIN, GOAL_WHITE_PIN]
+
+DEBOUNCING = 500
+
+
+def button_callback(channel):
+    if channel == GOAL_BLACK_PIN:
+        goal_id = GOAL_BLACK_ID
+    else:
+        goal_id = GOAL_WHITE_ID
+    url = f"{BACKEND_URL}/e/tor/{KICKER_ID}/{goal_id}"
+
+    resp = requests.post(url)
+    print(url)
+
+    if not resp.ok:
+        print("Error: {resp.status_code}")
+        print(resp.text)
+
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    for pin in PINS:
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(
+            pin, GPIO.RISING, callback=button_callback, bouncetime=DEBOUNCING)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    BACKEND_URL = args.backend_url
+    KICKER_ID = args.kid
+    setup()
+
+    loop = asyncio.new_event_loop()
+    loop.run_forever()

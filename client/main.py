414e09bef964dbc2645360685f097d276f0dfaf1
4KevR <50447092+4KevR@users.noreply.github.com>
2022-04-26 15:49:24 +0200

    added[client]: prevention to read same id twice

--- client/main.py
+++ client/main.py
@@ -1,11 +1,6 @@
 import time
-
-import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import requests
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
 
 
 def send_request(s_id, c_id) -> requests.Response:
@@ -22,34 +17,37 @@ class RfidReader:
         self.reader = [SimpleMFRC522(0), SimpleMFRC522(1)]
 
     def get_next_id(self) -> (int, int):
-        c_id = s_id = 0
-        while c_id == 0 or c_id is None:
-            for index, s_id in enumerate(self.scanner_ids):
+        c_id = s_id = None
+        while c_id is None:
+            for index, s_id in enumerate(self.scanner_ids):  # iterate over each reader
+
+                # read id from reader until time expiration
                 c_id = self.reader[index].read_id_no_block()
                 start_time = time.time()
                 while c_id is None and time.time() - start_time < 0.2:
                     c_id = self.reader[index].read_id_no_block()
-                if c_id is not None:
-                    print(f"Got id {c_id} from scanner {s_id}")
+
+                if c_id is not None and c_id != self.last_id:
+                    print(f"Got id {c_id} from scanner {s_id}")  # new id
                     break
+                elif c_id == self.last_id:
+                    c_id = None  # same id as last read
+
+        self.last_id = c_id
         return s_id, c_id
 
 
 if __name__ == "__main__":
-    try:
-        rfid_reader = RfidReader()
-        while True:
-            print("RFID-Reader ready...")
-            scanner_id, card_id = rfid_reader.get_next_id()
-            try:
-                resp = send_request(scanner_id, card_id)
-                if not resp.ok:
-                    print("Request failed\n")
-                else:
-                    print(f"Response: {resp.text}\n")
-            except requests.exceptions.ConnectionError:
-                print("Failed to establish a connection!\n")
-            time.sleep(1)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        raise
+    rfid_reader = RfidReader()
+    while True:
+        print("RFID-Reader ready...")
+        scanner_id, card_id = rfid_reader.get_next_id()
+        try:
+            resp = send_request(scanner_id, card_id)
+            if not resp.ok:
+                print("Request failed\n")
+            else:
+                print(f"Response: {resp.text}\n")
+        except requests.exceptions.ConnectionError:
+            print("Failed to establish a connection!\n")
+        time.sleep(0.2)

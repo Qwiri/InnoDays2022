9583ba9687976e70cd0f4bf12dbf92c920ac1b10
memeToasty <31214870+memeToasty@users.noreply.github.com>
2022-04-27 13:16:33 +0200

    Update client/start.sh
    Co-authored-by: Daniel <71837281+darmiel@users.noreply.github.com>

--- client/start.sh
+++ client/start.sh
@@ -2,4 +2,4 @@
 
 # Start client sripts
 screen -UmdS rfid-scan python3 rfid.py
-screen -UmdS goal-1 python3 goals.py --background-url "http://localhost" --kid "1"
+screen -UmdS goal-1 python3 goals.py --backend-url "http://localhost" --kid "1"

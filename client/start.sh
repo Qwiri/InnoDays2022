69b4971e5eea7c9df12593b2cea874c5d44d9527
Kevin Wiesner <50447092+4KevR@users.noreply.github.com>
2022-04-28 12:21:29 +0200

    added[client]: start chrome from script & use .env in start.sh (#16)
    * added[client]: start chrome from script & use .env in start.sh
    
    * refactor[client]: renamed environment variable
    
    * refactor[client]: added .env to gitignore & created .env.example
    
    * refactor[client]: moved chromium task to screen
    
    Co-authored-by: Daniel <71837281+darmiel@users.noreply.github.com>
    
    * refactor[client]: reversed chrome change
    
    Co-authored-by: Daniel <71837281+darmiel@users.noreply.github.com>

--- client/start.sh
+++ client/start.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-# Start client sripts
+# Load environment variables
+if [ -f .env ]; then
+    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+fi
+
+# Start client scripts
 screen -UmdS rfid-scan python3 rfid.py
-screen -UmdS goal-1 python3 goals.py --backend-url "http://localhost" --kid "1"
+screen -UmdS goal-1 python3 goals.py --backend-url ${BACKEND_URL} --kid ${KICKER_ID}
+
+# Start chrome
+chromium-browser --start-fullscreen ${FRONTEND_URL} &

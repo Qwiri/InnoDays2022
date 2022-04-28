ceb223fc377cbf73ba827d8ab7463a3a3c199ec7
TomRomeo <Tom.Romeo.Doil@gmail.com>
2022-04-28 12:44:25 +0200

    refactor[frontend]: linter

--- frontend/components/GoalDisplay.tsx
+++ frontend/components/GoalDisplay.tsx
@@ -1,46 +1,46 @@
-import { Fragment } from "react"
-import styles from "../styles/goalDisplay.module.scss"
+import { Fragment } from "react";
+import styles from "../styles/goalDisplay.module.scss";
 
-export default function GoalDisplay({goals, startTime}: {goals: Array<Goal>, startTime: string}) {
+export default function GoalDisplay({ goals, startTime }: { goals: Array<Goal>, startTime: string }) {
 
-    let startTimeStamp = Date.parse(startTime)
+	const startTimeStamp = Date.parse(startTime);
 
 
-    // return goaltime as string: `mm ss`
-    function formatTime(time: string) {
+	// return goaltime as string: `mm ss`
+	function formatTime(time: string) {
 
-        let goalTime = Date.parse(time)
-        let diff = goalTime - startTimeStamp
+		const goalTime = Date.parse(time);
+		const diff = goalTime - startTimeStamp;
 
-        let minutes = Math.floor(diff / 60000)
-        let minutesString = minutes.toString().length < 2 ? "0"+minutes.toString() : minutes.toString()
-        let seconds = Math.floor((diff / 1000) % 60)
-        let secondsString = seconds.toString().length < 2 ? "0"+seconds.toString() : seconds.toString()
+		const minutes = Math.floor(diff / 60000);
+		const minutesString = minutes.toString().length < 2 ? "0" + minutes.toString() : minutes.toString();
+		const seconds = Math.floor((diff / 1000) % 60);
+		const secondsString = seconds.toString().length < 2 ? "0" + seconds.toString() : seconds.toString();
 
-        return `${minutesString}m ${secondsString}s`
+		return `${minutesString}m ${secondsString}s`;
 
-    }
+	}
 
-    function paintCircle(g: Goal, team: number) {
-        let circleColor = (g.Team == team ? 'bg-white' : 'bg-stone-500');
-        let offset = (team == 2 ? 'after:top-1/2' : 'after:bottom-1/2')
+	function paintCircle(g: Goal, team: number) {
+		const circleColor = (g.Team == team ? "bg-white" : "bg-stone-500");
+		const offset = (team == 2 ? "after:top-1/2" : "after:bottom-1/2");
 
-        return(<span className={`${styles.preserve3d} ${circleColor} relative w-4 h-4 rounded-full bg-white text-center after:absolute after:translate-z-1 after:w-1 after:h-full after:left-[calc(50%-0.1rem)] after:bg-stone-600 ${offset}`}></span>)
-    }
+		return (<span className={`${styles.preserve3d} ${circleColor} relative w-4 h-4 rounded-full bg-white text-center after:absolute after:translate-z-1 after:w-1 after:h-full after:left-[calc(50%-0.1rem)] after:bg-stone-600 ${offset}`}></span>);
+	}
 
-    return(
-        <div className="grid grid-flow-row grid-cols-3 content-start justify-items-center items-center gap-2">
+	return (
+		<div className="grid grid-flow-row grid-cols-3 content-start justify-items-center items-center gap-2">
 
-            {goals.map((g, i) => {
-                return(
-                    <Fragment key={i}>
-                        {paintCircle(g, 1)}
-                        <p className='h-4 leading-none'>{formatTime(g.Time)}</p>
-                        {paintCircle(g, 2)}
-                    </Fragment>
-                )
-            })}
-        </div>
-    )
+			{goals.map((g, i) => {
+				return (
+					<Fragment key={i}>
+						{paintCircle(g, 1)}
+						<p className='h-4 leading-none'>{formatTime(g.Time)}</p>
+						{paintCircle(g, 2)}
+					</Fragment>
+				);
+			})}
+		</div>
+	);
 
 }
--- frontend/pages/kickae/[id].tsx
+++ frontend/pages/kickae/[id].tsx
@@ -1,145 +1,145 @@
-import styles from '/styles/kickae.module.scss'
-import Image from 'next/image'
-import { useEffect, useLayoutEffect, useState } from 'react'
-import TeamCard from '../../components/TeamCard';
-import GoalDisplay from '../../components/GoalDisplay';
+import styles from "/styles/kickae.module.scss";
+import Image from "next/image";
+import { useEffect, useLayoutEffect, useState } from "react";
+import TeamCard from "../../components/TeamCard";
+import GoalDisplay from "../../components/GoalDisplay";
 
-import {QRCodeSVG} from 'qrcode.react';
-import { useRouter } from 'next/router'
+import {QRCodeSVG} from "qrcode.react";
+import { useRouter } from "next/router";
 
 export default function KickaePage() {
-    const router = useRouter()
+	const router = useRouter();
 
-    const [scoreWhite, setScoreWhite] = useState("?");
-    const [scoreBlack, setScoreBlack] = useState("?");
-    const [teamWhite, setTeamWhite] = useState([]);
-    const [teamBlack, setTeamBlack] = useState([]);
-    const [goals, setGoals] = useState(Array<Goal>());
-    const [game, setGame] = useState();
+	const [scoreWhite, setScoreWhite] = useState("?");
+	const [scoreBlack, setScoreBlack] = useState("?");
+	const [teamWhite, setTeamWhite] = useState([]);
+	const [teamBlack, setTeamBlack] = useState([]);
+	const [goals, setGoals] = useState(Array<Goal>());
+	const [game, setGame] = useState();
 
-    const [qrCode, setQrCode] = useState("");
-    const [gameTime, setGameTime] = useState("");
+	const [qrCode, setQrCode] = useState("");
+	const [gameTime, setGameTime] = useState("");
 
-    function updateGameTime(startTime: number) {
+	function updateGameTime(startTime: number) {
 
-        let goalTime = Date.now();
-        let diff = goalTime - startTime;
+		const goalTime = Date.now();
+		const diff = goalTime - startTime;
 
-        let minutes = Math.floor(diff / 60000)
-        let minutesString = minutes.toString().length < 2 ? "0"+minutes.toString() : minutes.toString()
-        let seconds = Math.floor((diff / 1000) % 60)
-        let secondsString = seconds.toString().length < 2 ? "0"+seconds.toString() : seconds.toString()
+		const minutes = Math.floor(diff / 60000);
+		const minutesString = minutes.toString().length < 2 ? "0"+minutes.toString() : minutes.toString();
+		const seconds = Math.floor((diff / 1000) % 60);
+		const secondsString = seconds.toString().length < 2 ? "0"+seconds.toString() : seconds.toString();
 
-        setGameTime(`${minutesString}m ${secondsString}s`);
-    }
+		setGameTime(`${minutesString}m ${secondsString}s`);
+	}
 
-    async function fetchData() {
-        const res = await fetch("https://tnl2api.trd.moe/p/monitor/1");
-        const json = await res.json();
+	async function fetchData() {
+		const res = await fetch("https://tnl2api.trd.moe/p/monitor/1");
+		const json = await res.json();
         
-        let teamWhite: Array<Player> = [];
-        let teamBlack: Array<Player> = [];
-        let game: Game;
+		const teamWhite: Array<Player> = [];
+		const teamBlack: Array<Player> = [];
+		let game: Game;
 
-        if (json["Game"] === null) {
-            if (json["Pending"] !== null) {
+		if (json["Game"] === null) {
+			if (json["Pending"] !== null) {
 
-                let players: Array<Pending>
-                players = json["Pending"]
-                players.forEach((p) => {
-                    if (p.Pending.Team === 1) {
-                        teamBlack.push(p);
-                    } else if (p.Pending.Team === 2) {
-                        teamWhite.push(p);
-                    }
-                })
-            }
+				let players: Array<Pending>;
+				players = json["Pending"];
+				players.forEach((p) => {
+					if (p.Pending.Team === 1) {
+						teamBlack.push(p);
+					} else if (p.Pending.Team === 2) {
+						teamWhite.push(p);
+					}
+				});
+			}
 
-        } else {
-            game = json.Game
-            updateGameTime(Date.parse(game.StartTime));
+		} else {
+			game = json.Game;
+			updateGameTime(Date.parse(game.StartTime));
 
-            let players = game.Players;
-            players.forEach((p) => {
-                if (p.Team === 1) {
-                    teamBlack.push(p);
-                } else if (p.Team === 2) {
-                    teamWhite.push(p);
-                }
-            })
-        }
+			const players = game.Players;
+			players.forEach((p) => {
+				if (p.Team === 1) {
+					teamBlack.push(p);
+				} else if (p.Team === 2) {
+					teamWhite.push(p);
+				}
+			});
+		}
 
-        setTeamWhite(teamWhite);
-        setTeamBlack(teamBlack);
-        setGame(game);
-        setGoals(game?.Goals || []);
-        setScoreBlack(game?.ScoreBlack?.toString() || "?");
-        setScoreWhite(game?.ScoreWhite?.toString() || "?");
+		setTeamWhite(teamWhite);
+		setTeamBlack(teamBlack);
+		setGame(game);
+		setGoals(game?.Goals || []);
+		setScoreBlack(game?.ScoreBlack?.toString() || "?");
+		setScoreWhite(game?.ScoreWhite?.toString() || "?");
 
-    }
+	}
 
-    useEffect(() => {
-        setQrCode(window.location.href);
-        const interval = setInterval(() => {
-            fetchData();
-        }, 1000);
+	useEffect(() => {
+		setQrCode(window.location.href);
+		const interval = setInterval(() => {
+			fetchData();
+		}, 1000);
 
-        // clear interval on re-render to avoid memory leaks
-        return () => clearInterval(interval);
-    }, [])
+		// clear interval on re-render to avoid memory leaks
+		return () => clearInterval(interval);
+	}, []);
 
-    return(
-        <>
-            <div className="absolute top-0 left-0 w-full overflow-hidden">
-                <svg className="relative block h-16 w-full" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="fill-white"></path>
-                </svg>
-            </div>
-            <div className="p-8 mt-16 flex gap-4 text-4xl">
-                {/* QR-code */}
-                <QRCodeSVG size={64} value={qrCode} />
-                <div className='flex-column'>
-                    <h1>Game in progress</h1>
-                    {game?.ID === undefined ? null : <h3 className='text-gray-500 text-2xl'>#{game.ID}</h3> }
-                </div>
-            </div>
-            {/* wrapper */}
-            <div className='flex justify-around'>
-                {/* Team white */}
-                <div className='flex flex-col gap-8'>
+	return(
+		<>
+			<div className="absolute top-0 left-0 w-full overflow-hidden">
+				<svg className="relative block h-16 w-full" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+					<path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="fill-white"></path>
+				</svg>
+			</div>
+			<div className="p-8 mt-16 flex gap-4 text-4xl">
+				{/* QR-code */}
+				<QRCodeSVG size={64} value={qrCode} />
+				<div className='flex-column'>
+					<h1>Game in progress</h1>
+					{game?.ID === undefined ? null : <h3 className='text-gray-500 text-2xl'>#{game.ID}</h3> }
+				</div>
+			</div>
+			{/* wrapper */}
+			<div className='flex justify-around'>
+				{/* Team white */}
+				<div className='flex flex-col gap-8'>
 
-                    {/* Point display */}
-                    <div className='w-64 h-64 rounded-lg flex justify-center items-center bg-gradient-to-b from-gray-200 via-gray-400 to-gray-600'>
-                        <h1 className='text-8xl font-bold'>{scoreWhite}</h1>
-                    </div>
+					{/* Point display */}
+					<div className='w-64 h-64 rounded-lg flex justify-center items-center bg-gradient-to-b from-gray-200 via-gray-400 to-gray-600'>
+						<h1 className='text-8xl font-bold'>{scoreWhite}</h1>
+					</div>
 
-                    {/* user cards */}
-                    <TeamCard team={teamWhite} />
+					{/* user cards */}
+					<TeamCard team={teamWhite} />
 
-                </div>
+				</div>
 
-                {/* goal display */}
-                {(goals.length != 0) ? <GoalDisplay goals={goals} startTime={game.StartTime} /> : null}
+				{/* goal display */}
+				{(goals.length != 0) ? <GoalDisplay goals={goals} startTime={game.StartTime} /> : null}
 
-                {/* Black team */}
-                <div className='flex flex-col gap-8'>
+				{/* Black team */}
+				<div className='flex flex-col gap-8'>
 
-                    {/* Point display */}
-                    <div className='w-64 h-64 rounded-lg flex justify-center items-center bg-gradient-to-b from-gray-200 via-gray-400 to-gray-600'>
-                        <h1 className='text-8xl font-bold text-black'>{scoreBlack}</h1>
-                    </div>
+					{/* Point display */}
+					<div className='w-64 h-64 rounded-lg flex justify-center items-center bg-gradient-to-b from-gray-200 via-gray-400 to-gray-600'>
+						<h1 className='text-8xl font-bold text-black'>{scoreBlack}</h1>
+					</div>
 
-                    {/* user cards */}
-                    <TeamCard team={teamBlack} />
+					{/* user cards */}
+					<TeamCard team={teamBlack} />
 
-                </div>
+				</div>
 
-            </div>
+			</div>
 
-            {/* current time */}
-            <h1 className="text-4xl text-center">{gameTime}</h1>
+			{/* current time */}
+			<h1 className="text-4xl text-center">{gameTime}</h1>
 
                 
-        </>
-    )
+		</>
+	);
 }

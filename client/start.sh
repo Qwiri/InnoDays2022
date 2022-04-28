625518a9a67951c9d98b6fc07924dffbb1da4eb7
4KevR <50447092+4kevr@users.noreply.github.com>
2022-04-28 10:48:40 +0200

    refactor[client]: renamed environment variable

--- client/start.sh
+++ client/start.sh
@@ -10,4 +10,4 @@ screen -UmdS rfid-scan python3 rfid.py
 screen -UmdS goal-1 python3 goals.py --backend-url ${BACKEND_URL} --kid ${KICKER_ID}
 
 # Start chrome
-chromium-browser --start-fullscreen ${BACKEND_FRONTEND_URL} &
+chromium-browser --start-fullscreen ${FRONTEND_URL} &
